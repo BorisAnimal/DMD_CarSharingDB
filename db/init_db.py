c8bf78cfa19dba2cf4f5858a2de8e8cec7550e99
BorisAnimal <iguru98@yandex.ru>
2017-11-28 05:41:50 +0300

    code refactored

--- db/init_db.py
+++ db/init_db.py
@@ -1,66 +1,112 @@
 import os
 from db import __data_provider as provider
 
-tables_file = "setup.sql"
+tables_file = "../setup.sql"
 locations = None
 customers = None
 
 
 def create_db(connector, db_name):
+    """
+        :return: creates empty database
+    """
     cursor = connector.cursor()
     cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
+    cursor.commit()
+    print("Database was successfully created")
 
 
 def drop_database(db_name: str):
-    os.remove(db_name)
+    """
+        removes db_name file from disk
+    """
+    try:
+        os.remove(db_name)
+        print("Database file was successfully deleted")
+    except os.error as e:
+        print(e.args[0])
 
 
 def create_tables(connector):
+    """
+        executes setup file which contains all commands to create tables and
+        also some additional insert commands needed for tasks from assignment
+    """
     cursor = connector.cursor()
     for i in __get_tables():
         cursor.execute(i)
-        print(i)
+    print("All tables was successfully created")
 
 
 def __get_tables():
+    """
+        :return: list of commands from setup.sql file
+    """
     r = []
     with open(tables_file) as f:
         r = f.read().split("\n\n")
     return r
 
 
+def check_tables_existance(cursor):
+    """
+        checks user tables existence by checking tables number
+        :return: true if more than one user table else otherwise
+    """
+    tbls = cursor.execute("""select * from sqlite_master""")
+    # WARNING!!! do not execute fetchall() twice!!!
+    if len(tbls.fetchall()) > 2:
+        return True
+    return False
+
+
 def fill_all(cursor):
+    """
+        fill all tables with trash values
+    """
     __fill_charging_stations(cursor)
     __fill_repair_stations(cursor)
     cars = __fill_cars(cursor)
     manager = __fill_employee(cursor)
     __fill_car_parks(cursor)
     __fill_customer_activity(cursor, cars, manager)
-
+    print("All tables was successfully filled with data")
     cursor.close()
 
 
+#############
+#####   BLOCK OF QUERY COMMANDS TO DB
+#############
+
+
 def __generate_new_location(cursor):
+    """
+        :return: id of currently added location
+    """
     global locations
+    # Initialize locations tuples
     if (locations == None):
         locations = provider.get_locations()
+    # Manage end of locations list
     if (len(locations) <= 0):
         locations = None
         print("\n\n\nWARNING!!!!HAVE NO MORE LOCATIONS!!! HARDCODE ANYMORE VALUES IN LISTS!!!!!!ACHTUNG!!!!\n\n\n")
         __generate_new_location(cursor)
+    # If all good, add new location to the table locations
     else:
         cursor.execute("""INSERT INTO locations 
                                 (state, city, street, house, zip_code, gpsX, gpsY)
                                   VALUES (?,?,?,?,?,?,?)""", locations.pop())
-        cursor.execute("select LAST_INSERT_ROWID()")
-        for i in cursor:
-            return i[0]
+        return __get_last_id(cursor)
 
 
 def __generate_new_customer(cursor):
+    """
+        :return: id of currently added customer
+    """
     global customers
     if (customers == None):
-        cursor = provider.get_customers()
+        customers = provider.get_customers()
     if (len(customers) <= 0):
         customers = None
         print("\n\n\nWARNING!!!!HAVE NO MORE LOCATIONS!!! HARDCODE ANYMORE VALUES IN LISTS!!!!!!ACHTUNG!!!!\n\n\n")
@@ -70,9 +116,7 @@ def __generate_new_customer(cursor):
         cursor.execute("""INSERT INTO customers 
                                     (name, date_of_birth, phone_number, full_name, lives_in)
                                       VALUES (?,?,?,?,LAST_INSERT_ROWID())""", customers.pop())
-        cursor.execute("select LAST_INSERT_ROWID()")
-        for i in cursor:
-            return i[0]
+        return __get_last_id(cursor)
 
 
 def __fill_charging_stations(cursor):
@@ -99,6 +143,9 @@ def __fill_car_parks(cursor):
 
 
 def __fill_cars(cursor):
+    """
+        :return: list of ids of currently added cars
+    """
     tp = provider.get_cars()
     car_ids = []
     for i in tp:
@@ -106,34 +153,34 @@ def __fill_cars(cursor):
         cursor.execute("""INSERT INTO  cars
                             (state, charge_level, tarif, model, color,plate, location)
                               VALUES (?,?,?,?,?,?,LAST_INSERT_ROWID())""", i)
-        cursor.execute("select LAST_INSERT_ROWID()")
-        for i in cursor:
-            car_ids.append(i[0])
+        car_ids.append(__get_last_id(cursor))
     return car_ids
 
 
 def __fill_employee(cursor):
+    """
+        :return: id of currently added employee
+    """
     cursor.execute("""INSERT INTO posts (post_name, salary) VALUES (?,?)""", ("manager", 1400))
-    cursor.execute("select LAST_INSERT_ROWID()")
-    man = -1
-    for i in cursor:
-        man = i[0]
+    man = __get_last_id(cursor)
     cursor.execute("""INSERT INTO posts (post_name, salary) VALUES (?,?)""", ("telephone operator", 1000))
-    cursor.execute("select LAST_INSERT_ROWID()")
-    tel = -1
-    for i in cursor:
-        tel = i[0]
+    tel = __get_last_id(cursor)
     cursor.execute("""INSERT INTO employee (SSN, full_name,  phone_number, post_id) VALUES (?,?,?,?)""",
                    ("432101209", "Gager Nine", "88175522111", tel))
     cursor.execute("""INSERT INTO employee (SSN, full_name,  phone_number, post_id) VALUES (?,?,?,?)""",
                    ("902101234", "Cristofer Gag", "88005553535", man))
     # manager id in employee table
-    cursor.execute("select LAST_INSERT_ROWID()")
-    for i in cursor:
-        return i[0]
+    return __get_last_id(cursor)
 
 
 def __fill_customer_activity(cursor, car_ids: list, manager):
+    """
+        Fill tables:
+            * order
+            * feedback
+            * payments
+        with trash data of one exemplar
+    """
     # ORDER
     begin = "2017-11-24 00:00:00"
     end = "2017-11-24 00:10:00"
@@ -145,12 +192,19 @@ def __fill_customer_activity(cursor, car_ids: list, manager):
     cursor.execute(
         """insert into orders (start_time, end_time, made_by, included_car, start_location, destination) 
             VALUES (?,?,?,?,?,?)""", (start, end, cust, car_id, start, finish))
-    ord = ''
-    cursor.execute("select LAST_INSERT_ROWID()")
-    for i in cursor:
-        ord = i[0]
+    ord = __get_last_id(cursor)
     # Feedback
     cursor.execute("""insert into feedbacks (content, grade, managed_by, leaved_by) VALUES (?,?,?,?)""",
                    ("All was good", 10, manager, cust))
     # Payments
     cursor.execute("""insert into payments (paid_for_order, paid_amount) VALUES (?,?)""", (ord, 120))
+
+
+def __get_last_id(cursor):
+    """
+        :return: id of last added (sequentially, not use in multithreading) row in something table
+        (not matter which table)
+    """
+    cursor.execute("select LAST_INSERT_ROWID()")
+    for i in cursor:
+        return i[0]
--- main/main.py
+++ main/main.py
@@ -1,37 +1,92 @@
 import sqlite3
 from db import init_db
 
-db_name = 'CarSharingDB.db'
+db_name = '../CarSharingDB.db'
 conn = None
 
 
 def create_tables():
-    __check_connector()
-    init_db.create_tables(connector=conn)
+    """
+        creates schema of all tables with empty rows
+    """
+    try:
+        __check_connector()
+        init_db.create_tables(connector=conn)
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])
 
 
 def drop_database():
-    cr_connector = sqlite3.connect(db_name)
-    init_db.drop_database(db_name=db_name)
-    cr_connector.close()
+    """
+        deletes database file from disk
+    """
+    try:
+        cr_connector = sqlite3.connect(db_name)
+        init_db.drop_database(db_name=db_name)
+        cr_connector.close()
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])
 
 
 def close():
+    """
+        commits connection results and closes it
+    """
+    global conn
     if (conn != None):
+        conn.commit()
         conn.close()
+        conn = None
 
 
 def __check_connector():
+    """
+        initiate connection if it None
+    """
     global conn
     if (conn == None):
-        conn = sqlite3.connect(database=db_name)
+        try:
+            conn = sqlite3.connect(database=db_name)
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
 
 
-def main():
-    drop_database()
+def refresh_db():
+    """
+        drop database and create all from zero, also fill it with sample data
+    """
+    try:
+        drop_database()
+        __check_connector()
+        create_tables()
+        init_db.fill_all(conn.cursor())
+        conn.commit()
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])
+
+
+def execute(query: str):
+    """
+        execute user defined command
+    """
     __check_connector()
-    create_tables()
-    init_db.fill_all(conn.cursor())
-    conn.commit()
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        for i in cursor:
+            print(i)
+        print("Execution succeed")
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])
 
-    close()
+
+if __name__ == "__main__":
+    try:
+        refresh_db()  # TODO: remove this command
+        # create connection
+        __check_connector()
+        # check tables existence
+        if not init_db.check_tables_existance(conn.cursor()):
+            create_tables()
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])
